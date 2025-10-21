9889a79acef2b1ea652533342c713ad278397f2b
reborafs <reborafs@gmail.com>
2025-10-20 22:26:24 -0300

    chat con profesional

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { LoginScreen } from './src/screens/LoginScreen';
+import { ChatScreen } from './src/screens/ChatScreen';
 import { RegisterScreen } from './src/screens/RegisterScreen';
 import { SearchProfessionalsScreen } from './src/screens/SearchProfessionalsScreen';
 import { ProfileUserScreen } from './src/screens/ProfileUserScreen';
@@ -22,6 +23,7 @@ export default function App() {
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name="SearchProfessionals" component={SearchProfessionalsScreen} />
+        <Stack.Screen name="ChatScreen" component={ChatScreen} />
         <Stack.Screen name="ProfileUser" component={ProfileUserScreen} />
         <Stack.Screen name="ProfileProfessional" component={ProfileProfessionalScreen} />
       </Stack.Navigator>
