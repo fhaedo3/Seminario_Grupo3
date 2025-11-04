7ca3c4a96bf3d5d574b9532fb134bc4d8bc6e1e3
reborafs <reborafs@gmail.com>
2025-11-03 22:01:37 -0300

    fix

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://192.168.0.191:8080/api/v1";
+export const API_BASE_URL = "http://10.0.2.2:8080/api/v1";
 
 export const withBaseUrl = (path = '') => {
   if (!path) {
